fff998feb4dc6b7f49a2cd4341a9781baa315800
Christian Aistleitner <christian.aistleitner@outlook.com>
2016-03-30 12:22:53 +0200

    Add SHA256

--- App/wheel.cs
+++ App/wheel.cs
@@ -17,11 +17,27 @@ namespace Roulette_App
             //string server_seed = "39b7d32fcb743c244c569a56d6de4dc27577d6277d6cf155bdcba6d05befcb34";
             //string lotto = "0422262831";
             //string round_id = "1";
-            SHA256 sha = SHA256.Create("39b7d32fcb743c244c569a56d6de4dc27577d6277d6cf155bdcba6d05befcb34-0422262831-1");
-            string hash = sha.Hash.ToString();
+            //SHA256 sha = SHA256.Create("39b7d32fcb743c244c569a56d6de4dc27577d6277d6cf155bdcba6d05befcb34-0422262831-1");
+            string hash = sha256_hash("39b7d32fcb743c244c569a56d6de4dc27577d6277d6cf155bdcba6d05befcb34-0422262831-1");
             //string roll = hexdec(substr($hash, 0, 8)) % 15;
             MessageBox.Show(hash);
             //MessageBox.Show("Round $round_id = $roll");
+        }
+
+        private static String sha256_hash(String value)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (Byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
         }
     }
 }
