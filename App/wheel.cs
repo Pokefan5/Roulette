7a491dc6a87aa972fa568dcfb094c4f651bc77c6
Christian Aistleitner <christian.aistleitner@outlook.com>
2016-04-21 09:24:10 +0200

    Add comments to wheel.cs
    Commenting out every method and important waypoints

--- App/wheel.cs
+++ App/wheel.cs
@@ -1,32 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Roulette_App
 {
+    /// <summary>
+    /// Roulette table wheel
+    /// </summary>
     class Wheel
     {
+        /// <summary>
+        /// The count of fields of a roulette table
+        /// </summary>
         private uint NumberOfFields = 37;
 
+        /// <summary>
+        /// A random generating string for replying
+        /// </summary>
         private string seed;
 
+        /// <summary>
+        /// The index of the current round
+        /// </summary>
         private int round = 0;
 
+        /// <summary>
+        /// Initializise components
+        /// </summary>
         public Wheel()
         {
-            this.seed = getSeed();
+            byte[] buffer = new byte[128 / 2];
+            Random random = new Random();
+            random.NextBytes(buffer);
+            this.seed = String.Concat(buffer.Select(x => x.ToString("X2")).ToArray());
         }
 
+        /// <summary>
+        /// Play a round
+        /// </summary>
+        /// <returns>The played number</returns>
         public uint Play()
         {
             // get round Hash
-            string hash = getSHA256Hash(this.seed + "-" + this.round);
+            string hash = computeHash(this.seed + "-" + this.round);
 
-            // rise round counter
+            // increase round counter
             this.round++;
 
             // get the first byte
@@ -35,27 +54,23 @@ namespace Roulette_App
             return Convert.ToUInt32(hex, 16) % NumberOfFields;
         }
 
-        private string getSeed()
-        {
-            byte[] buffer = new byte[128 / 2];
-            Random random = new Random();
-            random.NextBytes(buffer);
-            return String.Concat(buffer.Select(x => x.ToString("X2")).ToArray());
-        }
-
-        private static String getSHA256Hash(String value) //generate random string (hex)
+        /// <summary>
+        /// Generate a SHA256 hash
+        /// </summary>
+        /// <param name="value">The round string</param>
+        /// <returns>The round hex</returns>
+        private static String computeHash(String value)
         {
             StringBuilder Sb = new StringBuilder();
+            SHA256 hash = SHA256.Create();
 
-            using (SHA256 hash = SHA256Managed.Create())
-            {
-                Encoding enc = Encoding.UTF8;
-                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+            // compute hash
+            Byte[] result = hash.ComputeHash(Encoding.UTF8.GetBytes(value));
 
-                foreach (Byte b in result)
-                    Sb.Append(b.ToString("x2"));
-            }
-
+            // convert hash to HEX string
+            foreach (Byte b in result)
+                Sb.Append(b.ToString("x2")); // "x2" for 2 HEX Characters, e.g. 11110000 = F0
+        
             return Sb.ToString();
         }
     }
